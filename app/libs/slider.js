486f17c8d69a6e89fc7a025740b7e5e57b8ecae2
olegsorokin <style-x55@mail.ru>
2017-06-28 19:12:59 +0600

    Изменил функцию рассчёта ширины элемента слайдера

--- app/libs/slider.js
+++ app/libs/slider.js
@@ -1,12 +1,18 @@
 const getClass = indexClass => document.getElementsByClassName(indexClass);
 const getId = indexId => document.getElementById(indexId);
-const getMargin = (element, margin) => Number(window.getComputedStyle(element).getPropertyValue(margin).match(/\d+/)[0]);
 
 const sliderItems = getClass('slider__item');
 const sliderLength = sliderItems.length;
-const sliderImg = getClass('slider__img')[0];
 
-const slideWidth = sliderImg.width + getMargin(sliderImg, 'margin-left') + getMargin(sliderImg, 'margin-right');
+const outerWidth = (el) => {
+  let width = el.offsetWidth;
+  const style = getComputedStyle(el);
+
+  width += parseInt(style.marginLeft) + parseInt(style.marginRight);
+  return width;
+};
+
+const slideWidth = outerWidth(getClass('slider__img')[0]);
 
 const insert = (first, last) => getId('slider__body').insertBefore(first, last);
 
@@ -17,7 +23,7 @@ const sliderAnimate = (speed = 15, delay = 0) => {
     margin -= 1;
     sliderItems[0].style.marginLeft = String(margin) + 'px';
     setTimeout(() => sliderAnimate(speed, delay), speed);
-  } else if (margin === -slideWidth) {
+  } else if (margin <= -slideWidth) {
     sliderItems[0].style.marginLeft = '0px';
     insert(sliderItems[0], sliderItems[sliderLength + 1]);
     margin = 0;
